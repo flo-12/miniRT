06dad6277db950c8a24fc0ac98024087951bdd3b
Florian Becht <fbecht@c4b10c2.42berlin.de>
2023-08-01 19:13:19 +0200

    utils.c added

---
+++ srcs/utils.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbecht <fbecht@student.42berlin.de>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/01 19:11:40 by fbecht            #+#    #+#             */
+/*   Updated: 2023/08/01 19:11:43 by fbecht           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "miniRT.h"
+
+/* free_ptr:
+*	Frees all strings ptr is pointing at and ptr otself.
+*	ptr ends on a NULL-pointer.
+*/
+void	free_ptr(char **ptr)
+{
+	char	**tmp;
+
+	tmp = ptr;
+	if (!ptr)
+		return ;
+	while (*ptr)
+	{
+		free(*ptr);
+		ptr++;
+	}
+	free(tmp);
+}
