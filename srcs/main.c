9d5273467decacdb07e1de9acaf5d44f28bdff77
flo-12 <flobecht@hotmail.com>
2023-08-03 14:35:53 +0200

    integrated parser in main

--- srcs/main.c
+++ srcs/main.c
@@ -12,8 +12,36 @@
 
 #include "miniRT.h"
 
+/* initialize_global:
+*	Initialize the global struct.
+*
+*	Return: exit code in case of error and
+*		otherwise success.
+*/
+int	initialize_global(t_global **global)
+{
+	*global = malloc(sizeof(t_global));
+	if (!(*global))
+		return (MALLOC_ERROR);
+	(*global)->light = NULL;
+	(*global)->camera = NULL;
+	(*global)->ambient = NULL;
+	(*global)->objects = NULL;
+	return (SUCCESS);
+}
+
 int	main(int argc, char **argv)
 {
-	
+	int			e;
+	t_global	*global;
+
+	if (argc != 2)
+		return (USER_ERROR);
+	e = initialize_global(&global);
+	if (e != SUCCESS)
+		return (e); 
+	e = parse(argv[1], global);
+	if (e != SUCCESS)
+		return (e);
 	return (EXIT_SUCCESS);
 }
